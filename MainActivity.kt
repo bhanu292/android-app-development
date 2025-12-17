41f2ef45f476fa4fa383ff3577bf9ad62f386da8
bhanu292 <bharat123tiwari123@gmail.com>
2025-12-17 20:47:12 +0530

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,33 +1,24 @@
-package com.example.implicitintent
+package com.example.explicitintent
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.net.URI
-
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val webButton = findViewById<CardView>(R.id.cardWeb)
-        val cameraButton = findViewById<CardView>(R.id.cardCamera)
+        val btnIntent = findViewById<Button>(R.id.button)
 
-        webButton.setOnClickListener {
-         val intent= Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse("https://github.com/bhanu292")
-            startActivity(intent)
-        }
-        cameraButton.setOnClickListener {
-        val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        btnIntent.setOnClickListener {
+            // Open a new Activity
+
+            intent= Intent(applicationContext, SecondActivity::class.java)
             startActivity(intent)
         }
 
