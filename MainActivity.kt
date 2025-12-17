d8d5182930e1a7a009a55b5394883caac5659602
bhanu292 <bharat123tiwari123@gmail.com>
2025-12-17 20:42:04 +0530

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,31 +1,30 @@
-package com.example.myandroid
+package com.example.interactiveapp
 
 import android.os.Bundle
+import android.text.Layout
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        var tvText = findViewById<TextView>(R.id.tvText)
-        var btnSubmit = findViewById<Button>(R.id.btnSubmit)
-        var etText = findViewById<EditText>(R.id.etText)
+        val buttonDark = findViewById<Button>(R.id.btnDark)
+        val buttonRead = findViewById<Button>(R.id.bthRead)
+        val layout = findViewById<LinearLayout>(R.id.linearLayout)
 
-       btnSubmit.setOnClickListener { var etString = etText.text.toString().trim()
-
-       tvText.text = "Hello $etString!"
-       }
-
-
+        buttonRead.setOnClickListener {
+            layout.setBackgroundResource(R.color.Yellow)
+        }
+        buttonDark.setOnClickListener {
+            layout.setBackgroundResource(R.color.black)
+        }
 
     }
 }
